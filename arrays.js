dac4408147a4db2c82edfef62fabf4374a8e5550
jacobplumblambda <dhagehassan@gmail.com>
2022-01-13 15:25:09 -0500

    something

--- arrays.js
+++ arrays.js
@@ -22,16 +22,49 @@
 
 // console.log(`the bottom ${a}`);
 
-let b = [1, 2, 3, 4];
-let newB = changeIndexTwo(b);
+// let b = [1, 2, 3, 4];
+// let newB = changeIndexTwo(b);
 
-function changeIndexTwo(x) {
-    const newArray = [...x];
-    newArray[2] = 9;
-    return newArray;
+// function changeIndexTwo(x) {
+//     const newArray = [...x];
+//     newArray[2] = 9;
+//     return newArray;
+// }
+
+// changeIndexTwo(b);
+
+// console.log(b);
+// console.log(newB);
+
+// let arrayOne = [1, 2, 3, 4];
+// console.log(arrayOne);
+// arrayOne.push(10);
+// console.log(arrayOne);
+// arrayOne.unshift(11);
+// console.log(arrayOne);
+// let end = arrayOne.pop();
+// console.log(arrayOne);
+// console.log(end);
+// let start = arrayOne.shift();
+// console.log(arrayOne);
+// console.log(start);
+// let middle = arrayOne.splice(2, 0, 12);
+// console.log(arrayOne);
+// console.log(middle);
+
+const myArr = [1, 2, 3, 4];
+
+function getSomething(arr) {
+    const s = [];
+    for (let i of arr) {
+        if (i !== 3) { 
+            console.log(i);
+        } else {
+            return 'blah';
+        }
+    }
+    console.log('something');
+
 }
 
-changeIndexTwo(b);
-
-console.log(b);
-console.log(newB);
+console.log(getSomething(myArr))
